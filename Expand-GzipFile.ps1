47330a947ab1bc91db088b0096fb3a82c994193d
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-03-01 14:46:50 -0300

    Create Expand-GzipFile.ps1

---
+++ Expand-GzipFile.ps1
@@ -0,0 +1,32 @@
+using namespace System
+using namespace System.Text
+using namespace System.IO
+using namespace System.IO.Compression
+
+function Expand-GzipFile {
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [Alias('FullName')]
+    [string]$Path,
+    [string]$Encoding = 'UTF8'
+)
+
+    try {
+        $Path = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Path)
+        $outStream = [MemoryStream]::new()
+        $handle = [FileInfo]::new($Path).OpenRead()
+        $gzip = [GZipStream]::new(
+            $handle,
+            [CompressionMode]::Decompress
+        )
+        $gzip.CopyTo($outStream)
+        [Encoding]::$Encoding.GetString($outStream.ToArray())
+    }
+    catch {
+        $PSCmdlet.WriteError($_)
+    }
+    finally {
+        ($gzip, $outStream, $handle).ForEach('Dispose')
+    }
+}
