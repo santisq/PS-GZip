68afc7f48b4aca3dc434ce5cf7e4a2017bdf9f95
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-06-06 01:41:42 -0300

    Update Expand-GzipFile.ps1

--- Expand-GzipFile.ps1
+++ Expand-GzipFile.ps1
@@ -5,12 +5,12 @@ using namespace System.IO.Compression
 
 function Expand-GzipFile {
 [cmdletbinding()]
-param(
-    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [Alias('FullName')]
-    [string]$Path,
-    [string]$Encoding = 'UTF8'
-)
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [string]$Path,
+        [string]$Encoding = 'UTF8'
+    )
 
     try {
         $Path = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Path)
